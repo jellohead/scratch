ccd50d766ca23520c193ca954fec34e11da2f4a9
Pat Escalona <pat.escalona7@gmail.com>
2022-10-21 21:01:09 -0500

    Initial commit

---
+++ scratch.js
@@ -0,0 +1,30 @@
+//scratch.js
+
+let myLibrary = [{
+    title: "book1",
+    author: "author1",
+    pages: "pages1",
+    read: "yes"
+},
+{
+    title: "book2",
+    author: "author2",
+    pages: "pages2",
+    read: "no"
+},
+{
+    title: "book3",
+    author: "author",
+    pages: "pages3",
+    read: "yes"
+}
+];
+
+let singleBook = {
+    title: "book1",
+    author: "author1",
+    pages: "pages1",
+    read: "yes"
+};
+
+Object.values(singleBook).forEach(val => console.log(val));
